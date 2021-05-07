8a908c70a56191f5586d01478983e95ccdf6b047
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-05-06 22:34:34 -0600

    Lab 12

---
+++ src/test/java/mx/tec/lab/TestSwingApplicationTests.java
@@ -0,0 +1,43 @@
+package mx.tec.lab;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSwingApplicationTests {
+	TestSwingApplication application = new TestSwingApplication();
+	
+	@Test
+	void givenTwoIntegers_WhenSum_ThenCorrectResult() {
+		// Given 5 and 7
+		application.numberOneTextField.setText("5");
+		application.numberTwoTextField.setText("7");
+		String expectedResult = "12";
+		
+		// When operation is sum
+		application.operationButton.doClick();
+		
+		// Then result is 12
+		String actualResult = application.resultTextField.getText();
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void givenTwoChars_WhenSum_ThenClearFields() {
+		// Given a and b
+		application.numberOneTextField.setText("a");
+		application.numberTwoTextField.setText("b");
+		String expectedResult = "";
+		
+		// When operation is sum
+		application.operationButton.doClick();
+		
+		// Then fields are cleared
+		String fieldOne = application.numberOneTextField.getText();
+		String fieldTwo = application.numberTwoTextField.getText();
+		assertEquals(expectedResult, fieldOne, fieldTwo);
+	}
+
+}
